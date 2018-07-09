ec8c3518723a91f85247ccaeba549549fcac9cf8
Jonathan Law Hui Hao <jonathan.law@cloudmails.apu.edu.my>
2018-07-09 20:18:37 +0800

    Tweaks

--- setup.sh
+++ setup.sh
@@ -8,6 +8,25 @@ sudo cp smb.conf /etc/samba/smb.conf
 echo Copying required NSSWITCH Config
 sudo cp nsswitch.conf /etc/nsswitch.conf
 echo -
-echo Enter your username to join domain
+echo Enter your username to join domain, eg jonathan.law
 read username
 net ads join -U $username
+echo - 
+echo -
+
+echo Making file share directory...
+echo Creating /home/jon/fileserver/pub
+echo Creating /home/jon/fileserver/sub
+sudo mkdir -p /home/jon/fileserver/{pub,sub}
+echo -
+
+echo Applying proper permissiong for domain user to access
+sudo chmod -R 0770 /home/jon/fileserver/
+sudo chgrp -R "Domain Users@techlab" /home/jon/fileserver
+echo -
+
+echo Starting services...
+sudo /etc/init.d/winbind restart
+sudo /etc/init.d/nmbd restart
+sudo /etc/init.d/smbd restart
+
